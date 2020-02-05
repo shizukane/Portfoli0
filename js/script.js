d4d3e6268807ed4d301dbc7783f5dd2e6a0d277e
shizukane <drizydarolle@gmail.com>
2020-02-06 00:10:18 +0300

    corrected misspellings

--- js/script.js
+++ js/script.js
@@ -1,49 +1,16 @@
 $(document).ready(function(){
-    $("#designimg , #design").click(function(){
-        $ ("#design").toggle()
-        $ ("#designimg").toggle()
+    $("#projectsdoneimg , #projectsdone").click(function(){
+        $ ("#projectsdone").toggle()
+        $ ("#projectsdoneimg").toggle()
     });
-    $("#developimg , #develop").click(function(){
-        $("#develop").toggle()
-        $("#developimg").toggle()
+    $("#hobbiesandgamesimg , #hobbiesandgames").click(function(){
+        $("#hobbiesandgames").toggle()
+        $("#hobbiesandgamesimg").toggle()
     });
-    $("#productimg , #product").click(function(){
-        $("#product").toggle()
-        $("#productimg").toggle()
+    $("#educationandspacialitiesimg , #educationandspecialities").click(function(){
+        $("#educationandspecialities").toggle()
+        $("#educationandspecialtiesimg").toggle()
     });
-
-    $(".myportfolio1").hover(function(){
-        $(".overlay1").toggle()
-    });
-    $(".myportfolio2").hover(function(){
-        $(".overlay2").toggle()
-    });
-    $(".myportfolio3").hover(function(){
-        $(".overlay3").toggle()
-    });
-    $(".myportfolio4").hover(function(){
-        $(".overlay4").toggle()
-    });
-    $(".myportfolio5").hover(function(){
-        $(".overlay5").toggle()
-    });
-    $(".myportfolio6").hover(function(){
-        $(".overlay6").toggle()
-    });
-    $(".myportfolio7").hover(function(){
-        $(".overlay7").toggle()
-    });
-    $(".myportfolio8").hover(function(){
-        $(".overlay8").toggle()
-    });
-
-    $("form#form").submit(function(){
-        var name = $("input#name").val();
-        var mail = $("input#email").val();
-        var message = $("textarea").val();
-        $ (alert("We have received your details.Thank you for your submission"))
-    });
-    
 });
 
 
