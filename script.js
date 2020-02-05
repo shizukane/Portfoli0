0ea930d9e1bea5e3c700bcc09e3b03d58ea2cb4f
shizukane <drizydarolle@gmail.com>
2020-02-05 15:15:12 +0300

    added js

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#designimg , #design").click(function(){
+        $ ("#design").toggle()
+        $ ("#designimg").toggle()
+    });
+    $("#developimg , #develop").click(function(){
+        $("#develop").toggle()
+        $("#developimg").toggle()
+    });
+    $("#productimg , #product").click(function(){
+        $("#product").toggle()
+        $("#productimg").toggle()
+    });
